b60b56e3472bc2bafd497cad7790004d5c3f26f3
jamroch79 <48738532+jamroch79@users.noreply.github.com>
2026-02-18 00:41:03 +0100

    Enable CORS in mail report server
    Added CORS support to the mail report server.

--- mail-report.js
+++ mail-report.js
@@ -1,7 +1,9 @@
 import nodemailer from "nodemailer";
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
