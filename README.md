4336bbf344bfb0f353904064fc88dbe13e5cdff0
jamroch79 <git@stackblitz.com>
2026-02-17 23:44:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# server-mail-radiotask-to-render
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/jamroch79/server-mail-radiotask-to-render)
---
+++ mail-report.js
@@ -0,0 +1,37 @@
+import nodemailer from "nodemailer";
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const transporter = nodemailer.createTransport({
+  host: "ssl0.ovh.net",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "panne@radiologie-lyon.com",
+    pass: process.env.OVH_MAIL_PASSWORD
+  }
+});
+
+app.post("/report", async (req, res) => {
+  const { subject, text } = req.body;
+
+  try {
+    await transporter.sendMail({
+      from: "panne@radiologie-lyon.com",
+      to: "panne@radiologie-lyon.com",
+      subject,
+      text
+    });
+
+    res.send("OK");
+  } catch (err) {
+    console.error("Mail error:", err);
+    res.status(500).send("Mail error");
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Mail report server running on port 3001");
+});
